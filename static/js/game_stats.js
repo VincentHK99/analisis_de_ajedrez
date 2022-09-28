0e159c7221b2a6a0e2a8f59ce77d1c41f061c2ab
VincentHK99 <vincenthemmingskavanagh@gmail.com>
2022-09-28 15:26:31 +0930

    referece to local .js file in static folder

---
+++ static/js/game_stats.js
@@ -0,0 +1,54 @@
+
+const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Win',
+        data: win_data,
+        backgroundColor: 'rgb(85,161,148)'
+        
+      },
+      {
+        label: 'Loss',
+        data: loss_data,
+        backgroundColor: 'rgb(241,106,111)'
+      
+      },        
+      {
+        label: 'Draw',
+        data: draw_data,
+        backgroundColor: 'rgb(152,160,166)'
+      },
+    ]
+  };
+  let delayed;
+  const config = {
+      type: 'bar',
+      data: data,
+      options: {
+        indexAxis: 'y',
+        plugins: {
+          title: {
+            display: true,
+            text: title
+          },
+        },
+        responsive: true,
+        scales: {
+          x: {
+            stacked: true,
+          },
+          y: {
+            stacked: true
+          }
+        }
+      }
+      };
+
+
+
+
+  const myChart = new Chart(
+      document.getElementById(element_id),
+      config
+      );
